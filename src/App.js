367e8aab3c9691266e464481db785bad695c657b
SixPlusOne7 <163210546+SixPlusOne7@users.noreply.github.com>
2025-10-13 21:54:22 -1000

    task 5

--- src/App.js
+++ src/App.js
@@ -9,16 +9,12 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
     if (calculateWinner(squares) || squares[i]) return;
     const next = squares.slice();
     next[i] = xIsNext ? "X" : "O";
-    setSquares(next);
-    setXIsNext(!xIsNext);
+    onPlay(next);
   }
 
   const winner = calculateWinner(squares);
@@ -45,6 +41,46 @@ export default function Board() {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
+  );
+}
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(move) {
+    setCurrentMove(move);
+  }
+
+  const moves = history.map((_, move) => {
+    const here = move === currentMove;
+    const label = move ? `Go to move #${move}` : "Go to game start";
+    return (
+      <li key={move}>
+        {here ? <span>You are at move #{move}</span> :
+                 <button onClick={() => jumpTo(move)}>{label}</button>}
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
   );
 }
 
